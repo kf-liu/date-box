f605b4af76940089827d02849b66b103c8559877
KefangLiu <kf_ch_liu@163.com>
2022-10-24 12:36:57 +0000

    fix: md format

--- test.js
+++ test.js
@@ -35,16 +35,27 @@ for (let i = 0; i <= 7; i += 1) {
 
 
 // contents
-const ending = '\n🤖 by [date-box](https://github.com/marketplace/actions/date-box)';
-const TODAY_INPUT = `${today.clone().format(FORMAT)} _ Today`;
-const TODAY_OUTPUT = '🎁🎁🎁 Today is Today';
-const YESTERDAY_INPUT = `${yesterday.clone().format(FORMAT)} _ Yesterday`;
-const YESTERDAY_OUTPT = '🗓 1 day  after  Yesterday';
-const TOMORROW_INPUT = `${tomorrow.clone().format(FORMAT)} _ Tomorrow`;
-const TOMORROW_OUTPT = '🗓 1 day  before Tomorrow';
+const ending = '🤖 by [date-box](https://github.com/marketplace/actions/date-box)';
+const TODAY_INPUT = `${today.clone().format(FORMAT)} _ TODAY`;
+const TODAY_OUTPUT = '🎁🎁🎁 Today is TODAY\n\n';
+const YESTERDAY_INPUT = `${yesterday.clone().format(FORMAT)} _ YESTERDAY`;
+const YESTERDAY_OUTPUT = '🗓 1 day  after  YESTERDAY\n\n';
+const YESTERDAY_OUTPUT_S = '🗓 1  day  after  YESTERDAY\n\n';
+const TOMORROW_INPUT = `${tomorrow.clone().format(FORMAT)} _ TOMORROW`;
+const TOMORROW_OUTPUT = '🗓 1 day  before TOMORROW\n\n';
+const TOMORROW_OUTPUT_S = '🗓 1  day  before TOMORROW\n\n';
 assert.equal(contents(TODAY_INPUT), TODAY_OUTPUT + ending);
-assert.equal(contents(YESTERDAY_INPUT), YESTERDAY_OUTPT + ending);
-assert.equal(contents(TOMORROW_INPUT), TOMORROW_OUTPT + ending);
-assert.equal(contents(`${TODAY_INPUT} | ${TODAY_INPUT}`), `${TODAY_OUTPUT}\n${TODAY_OUTPUT}` + ending);
-assert.equal(contents(`${TODAY_INPUT} | ${YESTERDAY_INPUT} | ${TOMORROW_INPUT}`), `${TODAY_OUTPUT}\n${YESTERDAY_OUTPT}\n${TOMORROW_OUTPT}` + ending);
-assert.equal(contents(`${YESTERDAY_INPUT} | ${TODAY_INPUT} | ${TOMORROW_INPUT}`), `${TODAY_OUTPUT}\n${YESTERDAY_OUTPT}\n${TOMORROW_OUTPT}` + ending);
+assert.equal(contents(YESTERDAY_INPUT), YESTERDAY_OUTPUT + ending);
+assert.equal(contents(TOMORROW_INPUT), TOMORROW_OUTPUT + ending);
+assert.equal(contents(`${TODAY_INPUT} | ${TODAY_INPUT}`), `${TODAY_OUTPUT}${TODAY_OUTPUT}` + ending);
+assert.equal(contents(`${TODAY_INPUT} | ${YESTERDAY_INPUT} | ${TOMORROW_INPUT}`), `${TODAY_OUTPUT}${YESTERDAY_OUTPUT}${TOMORROW_OUTPUT}` + ending);
+assert.equal(contents(`${YESTERDAY_INPUT} | ${TODAY_INPUT} | ${TOMORROW_INPUT}`), `${TODAY_OUTPUT}${YESTERDAY_OUTPUT}${TOMORROW_OUTPUT}` + ending);
+
+const TEN_DAYS_AFTER_INPUT = `${today.clone().subtract(10, 'day').format(FORMAT)} _ TEN_DAYS_AFTER`;
+const TEN_DAYS_AFTER_OUTPUT = `🗓 10 days after  TEN_DAYS_AFTER\n\n`;
+const TEN_DAYS_BEFORE_INPUT = `${today.clone().add(10, 'day').format(FORMAT)} _ TEN_DAYS_BEFORE`;
+const TEN_DAYS_BEFORE_OUTPUT = `🗓 10 days before TEN_DAYS_BEFORE\n\n`;
+
+const input = `${TEN_DAYS_AFTER_INPUT} | ${TODAY_INPUT} | ${YESTERDAY_INPUT} | 0 0 * * * _ TOMORROW | ${TEN_DAYS_BEFORE_INPUT} | ${TOMORROW_INPUT}`;
+const output = TODAY_OUTPUT + YESTERDAY_OUTPUT_S + TOMORROW_OUTPUT_S + TOMORROW_OUTPUT_S + TEN_DAYS_AFTER_OUTPUT + TEN_DAYS_BEFORE_OUTPUT + ending;
+assert.equal(contents(input), output);
